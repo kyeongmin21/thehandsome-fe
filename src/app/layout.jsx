b77ef67d01968676d64ed9aefc046a4ac75461d9
kim kyeong min <kyeongmin21@naver.com>
2025-11-17 14:47:20 +0900

    footer 추가

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,11 +5,13 @@ import Footer from "@/components/layout/Footer";
 export default function RootLayout({children}) {
     return (
         <html lang="en">
-            <body>
+        <body>
+            <div className="flex flex-col min-h-screen">
                 <Header/>
-                <div className='main-children'>{children}</div>
-                <Footer/>
-            </body>
+                <div className='main-children flex-1'>{children}</div>
+                <div className='h-[0px]'><Footer/></div>
+            </div>
+        </body>
         </html>
     );
 }
