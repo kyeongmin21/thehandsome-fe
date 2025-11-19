86d51edcc920ec3b66767d87c323988ffba686e9
kim kyeong min <kyeongmin21@naver.com>
2025-11-19 13:55:28 +0900

    [메인페이지] 위시리스트 개발

--- src/app/mypage/wishlist/page.jsx
+++ src/app/mypage/wishlist/page.jsx
@@ -1,6 +1,22 @@
 'use client'
+import {useEffect} from "react";
+import apiHelper from "@/utils/apiHelper";
 
 const WishList = () => {
+
+    const fetchData = async () => {
+        try {
+            const res = await apiHelper.get('/wishlist/my')
+            console.log('위시리스트 조회데이터', res)
+        } catch (error) {
+            console.error('마이페이지 위시리스트 조회 실패', error)
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
     return (
         <div>
             <h2>위시리스트</h2>
