b0c886ba3bbf8d9c7662a7e35ae97f7d1b590e3f
kim kyeong min <kyeongmin21@naver.com>
2025-12-07 23:17:56 +0900

    세션만료 로그아웃 처리

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,7 @@ import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import RootProviders from "@/app/provider/RootProvider";
 import ProgressBar from "@/components/common/ProgressBar";
+import AuthStatus from "@/components/AuthStatus";
 import {serverSession} from '@/utils/getServerSession';
 import {serverBrandLike} from "@/utils/getServerBrandLike";
 
@@ -17,6 +18,7 @@ export default async function RootLayout({children}) {
         <div className="flex flex-col min-h-screen">
             <RootProviders>
                 <ProgressBar/>
+                <AuthStatus />
                 <Header initSession={session} initBrandLike={brandLike}/>
                 <div className='main-children flex-1'>{children}</div>
                 <div className='h-[0px]'><Footer/></div>
