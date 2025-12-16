1195fcd9f98392d799371fde2168df23cb350270
kim kyeong min <kyeongmin21@naver.com>
2025-12-16 19:11:29 +0900

    lighthouse - seo 검색엔진 최적화

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,17 +5,28 @@ import RootProviders from "@/app/provider/RootProvider";
 import ProgressBar from "@/components/common/ProgressBar";
 import AuthStatus from "@/components/AuthStatus";
 
+export const metadata = {
+    title: {
+        default: "더한섬닷컴",
+        template: "", // 하위 페이지 제목이 있을 때 연결 방식
+    },
+    description: "사이드 프로젝트",
+    icons: {
+        icon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧸</text></svg>",
+    },
+};
+
 export default async function RootLayout({children}) {
 
     return (
-        <html lang="en">
+        <html lang="ko">
         <body>
         <div className="flex flex-col min-h-screen">
             <RootProviders>
                 <ProgressBar/>
                 <AuthStatus />
                 <Header/>
-                <div className='main-children flex-1'>{children}</div>
+                <main className='main-children flex-1'>{children}</main>
                 <div className='h-[0px]'><Footer/></div>
             </RootProviders>
         </div>
